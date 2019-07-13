3386003320cae3dbca6ceabb9cb014413aab6156
jszlenk <jakubszlenk@fastmail.com>
2019-07-13 20:42:44 +0200

    Wait and Notify

--- BasicMultithreading/src/WaitAndNotify/App.java
+++ BasicMultithreading/src/WaitAndNotify/App.java
@@ -6,28 +6,22 @@ public class App {
 
         Processor processor = new Processor();
 
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    processor.produce();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
+        Thread t1 = new Thread(() -> {
+            try {
+                processor.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+
         });
 
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    processor.consume();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
+        Thread t2 = new Thread(() -> {
+            try {
+                processor.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+
         });
 
         t1.start();
