bbfc923685386141a59f95c155cdb77449ef25b4
jszlenk <jakubszlenk@fastmail.com>
2019-08-01 22:14:23 +0200

    Exchanger

---
+++ BasicMultithreading/src/ConcurrentCollections/Exchanger/App.java
@@ -0,0 +1,13 @@
+package ConcurrentCollections.Exchanger;
+
+import java.util.concurrent.Exchanger;
+
+public class App {
+    public static void main(String[] args) {
+
+        Exchanger<Integer> exchanger = new Exchanger<>();
+
+        new Thread(new FirstWorker(exchanger)).start();
+        new Thread(new SecondWorker(exchanger)).start();
+    }
+}
---
+++ BasicMultithreading/src/ConcurrentCollections/Exchanger/FirstWorker.java
@@ -0,0 +1,35 @@
+package ConcurrentCollections.Exchanger;
+
+import java.util.concurrent.Exchanger;
+
+class FirstWorker implements Runnable {
+
+    private int counter;
+    private Exchanger<Integer> exchanger;
+
+    FirstWorker(Exchanger<Integer> exchanger) {
+        this.exchanger = exchanger;
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+
+            counter = counter + 1;
+            System.out.println("FirstWorker incremented the counter: " + counter);
+
+            try {
+                counter = exchanger.exchange(counter);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
